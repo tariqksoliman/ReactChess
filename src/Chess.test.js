505b740a35f449f958ab5dca82b1d3f9e84d4d87
tariqksoliman <tariqksoliman@gmail.com>
2017-06-05 21:02:27 -0700

    Updates

--- src/Chess.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Chess from './Chess';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Chess />, div);
-});
