a08b92b0b45fedb8e1e0d2481924f714ce82eea3
tariqksoliman <tariqksoliman@gmail.com>
2017-06-05 20:45:00 -0700

    Another 4hrs

--- src/Chess.js
+++ src/Chess.js
@@ -18,39 +18,67 @@ class Board extends Component {
     this.state = {
       layout: this.getInitialBoardLayout(),
       whitesTurn: true,
-      chosen: Array.from({length: 8}, () => Array.from({length: 8}, () => false) ),
+      chosen: [ null, null ],
+      //chosen: Array.from({length: 8}, () => Array.from({length: 8}, () => false) ),
       possible: Array.from({length: 8}, () => Array.from({length: 8}, () => false) )
     }
   }
 
   getInitialBoardLayout() {
+    // {side}{piece}{_ if hasn't moved yet}
     return [
-             [ 'rR', 'rN', 'rB', 'rQ', 'rK', 'rB', 'rN', 'rR' ],
-             [ 'rP', 'rP', 'rP', 'rP', 'rP', 'rP', 'rP', 'rP' ],
-             [  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  ''  ],
-             [  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  ''  ],
-             [  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  ''  ],
-             [  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  '' ,  ''  ],
-             [ 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP' ],
-             [ 'wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR' ]
+             [ 'rR_', 'rN_', 'rB_', 'rQ_', 'rK_', 'rB_', 'rN_', 'rR_' ],
+             [ 'rP_', 'rP_', 'rP_', 'rP_', 'rP_', 'rP_', 'rP_', 'rP_' ],
+             [   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   ''  ],
+             [   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   ''  ],
+             [   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   ''  ],
+             [   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   '' ,   ''  ],
+             [ 'wP_', 'wP_', 'wP_', 'wP_', 'wP_', 'wP_', 'wP_', 'wP_' ],
+             [ 'wR_', 'wN_', 'wB_', 'wQ_', 'wK_', 'wB_', 'wN_', 'wR_' ]
            ];
   }
 
   handleClick( x, y ) {
-    
-    const chosen = Array.from({length: 8}, () => Array.from({length: 8}, () => false));
+
+    const layout = this.state.layout.slice();
+    var whitesTurn = this.state.whitesTurn;
+    var chosen = [ null, null ];
     const possible = Array.from({length: 8}, () => Array.from({length: 8}, () => false));
     const movements = pieceMovements( x, y, this.state.layout ) || [];
 
-    if( ( this.state.whitesTurn && this.state.layout[y][x][0] === 'w' ) ||
-        ( !this.state.whitesTurn && this.state.layout[y][x][0] === 'r' ) ) {
-      chosen[y][x] = true;
+    //if you click your piece
+    if( ( whitesTurn && this.state.layout[y][x][0] === 'w' ) ||
+        ( !whitesTurn && this.state.layout[y][x][0] === 'r' ) ) {
+      chosen = [ x, y ];
       for( let i = 0; i < movements.length; i++ ) {
         possible[ movements[i][1] ][ movements[i][0] ] = true;
       }
     }
+    //or if you click a place to move
+    else if( this.state.possible[y][x] ) {
+      //was it a castle
+      if( layout[ this.state.chosen[1] ][ this.state.chosen[0] ].substring( 1, 3 ) === 'K_' ) {
+        if( x === 2 ) {
+          layout[y][3] = this.state.layout[y][0].substring( 0, 2 );
+          layout[y][0] = '';
+        }
+        else if( x === 6 ) {
+          layout[y][5] = this.state.layout[y][7].substring( 0, 2 );
+          layout[y][7] = '';
+        }
+      }
+
+      //substr02 to remove hasnt moved underscore indicator
+      layout[y][x] = this.state.layout[ this.state.chosen[1] ][ this.state.chosen[0] ].substring( 0, 2 );
+      layout[ this.state.chosen[1] ][ this.state.chosen[0] ] = '';
+
+      whitesTurn = !whitesTurn;
+      document.body.className = ( whitesTurn ) ? 'whitesTurn' : 'redsTurn';
+    }
 
     this.setState({
+      layout: layout,
+      whitesTurn: whitesTurn,
       chosen: chosen,
       possible: possible
     });
@@ -62,6 +90,9 @@ class Board extends Component {
       var classes = 'square color' + (x+y)%2;
       return classes;
     }
+
+    const chosen = ( x === this.state.chosen[0] &&
+                     y === this.state.chosen[1] ) ? true : false;
     
     return (
       <Square
@@ -70,7 +101,7 @@ class Board extends Component {
         posY = { y }
         classes = {getClasses()}
         piece = {this.state.layout[y][x]}
-        chosen = {this.state.chosen[y][x]}
+        chosen = {chosen}
         possible = {this.state.possible[y][x]}
         onClick={() => this.handleClick( x, y )}
       />
@@ -111,11 +142,20 @@ class Square extends Component {
 }
 
 class Piece extends Component {
-  handlePieceClick( active ) {
-    if( active === ' active' ) {
+  handlePieceClick() {
       this.props.onClick();
-    }
   }
+
+  mapTypeToPieceCode( type, side ) {
+    if( type === 'P' ) return 'o';
+    if( type === 'R' ) return 't';
+    if( type === 'N' ) return 'j';
+    if( type === 'B' ) return 'n';
+    if( type === 'Q' ) return 'w';
+    if( type === 'K' ) return 'l';
+    return '';
+  }
+
   render() {
     const chosen = this.props.chosen ? ' chosen' : '';
     const possible = this.props.possible ? ' possible' : '';
@@ -124,9 +164,9 @@ class Piece extends Component {
     const type = this.props.piece !== '' ? ' ' + this.props.piece[1] : '';
     return (
       <div className={ 'piece' + side + type + chosen + possible + active }
-           onClick={() => this.handlePieceClick( active ) }
+           onClick={() => this.handlePieceClick( ) }
       >
-        {type}
+        {this.mapTypeToPieceCode( type[1], side[1] )}
       </div>
     );
   }
--- src/ChessMoves.js
+++ src/ChessMoves.js
@@ -5,6 +5,7 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
     
     var side = pieceId[0].toLowerCase();
     var type = pieceId[1].toLowerCase();
+    var hasntMoved = pieceId.length === 3 && pieceId[2] === '_';
 
     switch( type ) {
         case 'p':
@@ -22,6 +23,8 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
         default:
             console.warn( 'Unknown piece type ' + type );
     }
+
+
 
     function movePawn() {
         var movements = [];
@@ -47,9 +50,6 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
         return movements;
         //===============================
 
-        function isInStartingPosition() {
-            return ( side === 'w' && pieceY === 6 ) || ( side === 'r' && pieceY === 1 );
-        }
         function leftDiagonalHasEnemy() {
             if( side === 'w' &&
                 onB( pieceX - 1, pieceY - 1 ) &&
@@ -72,7 +72,7 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
             else if( side === 'r' &&
                 onB( pieceX + 1, pieceY + 1 ) &&
                 layout[ pieceY + 1 ][ pieceX + 1 ][0] === 'w' ) {
-                return [ pieceX + 1, pieceY - 1 ];
+                return [ pieceX + 1, pieceY + 1 ];
             }
             return false;
         }
@@ -90,7 +90,7 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
             return false;
         }
         function getDoubleForward() {
-            if( !isInStartingPosition() ) return false;
+            if( !hasntMoved ) return false;
             if( side === 'w' ) {
                 if( isV( pieceX, pieceY - 1 ) && isV( pieceX, pieceY - 2 )  ) {
                     return [ pieceX, pieceY - 2 ];
@@ -104,25 +104,293 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
         }
     }
 
-    function moveRook() {
 
+    function moveRook() {
+        var movements = [];
+        
+        var m;
+
+        m = getUp();
+        if( m ) movements = movements.concat( m );
+
+        m = getLeft();
+        if( m ) movements = movements.concat( m );
+
+        m = getDown();
+        if( m ) movements = movements.concat( m );
+
+        m = getRight();
+        if( m ) movements = movements.concat( m );
+
+        return movements;
+        //================
+        //check up left down right
+        function getUp() {
+            var submovements = [];
+            var curY = pieceY - 1;
+            while( isV( pieceX, curY ) ) {
+                submovements.push( [ pieceX, curY ] );
+                curY--;
+            }
+            if( isE( pieceX, curY ) )
+                submovements.push( [ pieceX, curY ] );
+            return submovements;
+        }
+        function getLeft() {
+            var submovements = [];
+            var curX = pieceX - 1;
+            while( isV( curX, pieceY ) ) {
+                submovements.push( [ curX, pieceY ] );
+                curX--;
+            }
+            if( isE( curX, pieceY ) )
+                submovements.push( [ curX, pieceY ] );
+            return submovements;
+        }
+        function getDown() {
+            var submovements = [];
+            var curY = pieceY + 1;
+            while( isV( pieceX, curY ) ) {
+                submovements.push( [ pieceX, curY ] );
+                curY++;
+            }
+            if( isE( pieceX, curY ) )
+                submovements.push( [ pieceX, curY ] );
+            return submovements;
+        }
+        function getRight() {
+            var submovements = [];
+            var curX = pieceX + 1;
+            while( isV( curX, pieceY ) ) {
+                submovements.push( [ curX, pieceY ] );
+                curX++;
+            }
+            if( isE( curX, pieceY ) )
+                submovements.push( [ curX, pieceY ] );
+            return submovements;
+        }
     }
+
 
     function moveKnight() {
+        var movements = [];
+        
+        var m;
 
+        m = getUpLeft();
+        if( m ) movements.push( m );
+
+        m = getUpRight();
+        if( m ) movements.push( m );
+
+        m = getLeftUp();
+        if( m ) movements.push( m );
+
+        m = getLeftDown();
+        if( m ) movements.push( m );
+
+        m = getDownLeft();
+        if( m ) movements.push( m );
+
+        m = getDownRight();
+        if( m ) movements.push( m );
+
+        m = getRightUp();
+        if( m ) movements.push( m );
+
+        m = getRightDown();
+        if( m ) movements.push( m );
+
+        return movements;
+        //==============
+        
+        //eight possible movements
+        function getUpLeft() {
+            if( isVE( pieceX - 1, pieceY - 2 ) ) return [ pieceX - 1, pieceY - 2 ];
+        }
+        function getUpRight() {
+            if( isVE( pieceX + 1, pieceY - 2 ) ) return [ pieceX + 1, pieceY - 2 ];
+        }
+        function getLeftUp() {
+            if( isVE( pieceX - 2, pieceY - 1 ) ) return [ pieceX - 2, pieceY - 1 ];
+        }
+        function getLeftDown() {
+            if( isVE( pieceX - 2, pieceY + 1 ) ) return [ pieceX - 2, pieceY + 1 ];
+        }
+        function getDownLeft() {
+            if( isVE( pieceX - 1, pieceY + 2 ) ) return [ pieceX - 1, pieceY + 2 ];
+        }
+        function getDownRight() {
+            if( isVE( pieceX + 1, pieceY + 2 ) ) return [ pieceX + 1, pieceY + 2 ];
+        }
+        function getRightUp() {
+            if( isVE( pieceX + 2, pieceY - 1 ) ) return [ pieceX + 2, pieceY - 1 ];
+        }
+        function getRightDown() {
+            if( isVE( pieceX + 2, pieceY + 1 ) ) return [ pieceX + 2, pieceY + 1 ];
+        }
     }
+
 
     function moveBishop() {
+        var movements = [];
+        
+        var m;
 
+        m = getUpLeft();
+        if( m ) movements = movements.concat( m );
+
+        m = getUpRight();
+        if( m ) movements = movements.concat( m );
+
+        m = getDownLeft();
+        if( m ) movements = movements.concat( m );
+
+        m = getDownRight();
+        if( m ) movements = movements.concat( m );
+
+        return movements;
+        //================
+        //check four diagonals
+        function getUpLeft() {
+            var submovements = [];
+            var curX = pieceX - 1;
+            var curY = pieceY - 1;
+            while( isV( curX, curY ) ) {
+                submovements.push( [ curX, curY ] );
+                curX--;
+                curY--;
+            }
+            if( isE( curX, curY ) )
+                submovements.push( [ curX, curY ] );
+            return submovements;
+        }
+        function getUpRight() {
+            var submovements = [];
+            var curX = pieceX + 1;
+            var curY = pieceY - 1;
+            while( isV( curX, curY ) ) {
+                submovements.push( [ curX, curY ] );
+                curX++;
+                curY--;
+            }
+            if( isE( curX, curY ) )
+                submovements.push( [ curX, curY ] );
+            return submovements;
+        }
+        function getDownLeft() {
+            var submovements = [];
+            var curX = pieceX - 1;
+            var curY = pieceY + 1;
+            while( isV( curX, curY ) ) {
+                submovements.push( [ curX, curY ] );
+                curX--;
+                curY++;
+            }
+            if( isE( curX, curY ) )
+                submovements.push( [ curX, curY ] );
+            return submovements;
+        }
+        function getDownRight() {
+            var submovements = [];
+            var curX = pieceX + 1;
+            var curY = pieceY + 1;
+            while( isV( curX, curY ) ) {
+                submovements.push( [ curX, curY ] );
+                curX++;
+                curY++;
+            }
+            if( isE( curX, curY ) )
+                submovements.push( [ curX, curY ] );
+            return submovements;
+        }
     }
+
 
     function moveQueen() {
-
+        return moveRook().concat( moveBishop() ); 
     }
+
 
     function moveKing() {
+        var movements = [];
 
+        var m;
+
+        m = getUpLeft();
+        if( m ) movements.push( m );
+
+        m = getUp();
+        if( m ) movements.push( m );
+
+        m = getUpRight();
+        if( m ) movements.push( m );
+
+        m = getRight();
+        if( m ) movements.push( m );
+
+        m = getDownRight();
+        if( m ) movements.push( m );
+
+        m = getDown();
+        if( m ) movements.push( m );
+
+        m = getDownLeft();
+        if( m ) movements.push( m );
+
+        m = getLeft();
+        if( m ) movements.push( m );
+
+        m = getCastleLeft();
+        if( m ) movements.push( m );
+
+        m = getCastleRight();
+        if( m ) movements.push( m );
+
+        return movements;
+        //==================
+
+        function getUpLeft() {
+            if( isVE( pieceX - 1, pieceY - 1 ) ) return [ pieceX - 1, pieceY - 1 ];
+        }
+        function getUp() {
+            if( isVE( pieceX, pieceY - 1 ) ) return [ pieceX, pieceY - 1 ];
+        }
+        function getUpRight() {
+            if( isVE( pieceX + 1, pieceY - 1 ) ) return [ pieceX + 1, pieceY - 1 ];
+        }
+        function getRight() {
+            if( isVE( pieceX + 1, pieceY ) ) return [ pieceX + 1, pieceY ];
+        }
+        function getDownRight() {
+            if( isVE( pieceX + 1, pieceY + 1 ) ) return [ pieceX + 1, pieceY + 1 ];
+        }
+        function getDown() {
+            if( isVE( pieceX, pieceY + 1 ) ) return [ pieceX, pieceY + 1 ];
+        }
+        function getDownLeft() {
+            if( isVE( pieceX - 1, pieceY + 1 ) ) return [ pieceX - 1, pieceY + 1 ];
+        }
+        function getLeft() {
+            if( isVE( pieceX - 1, pieceY ) ) return [ pieceX - 1, pieceY ];
+        }
+
+        //Castling
+        function getCastleLeft() {
+            if( hasntMoved && layout[pieceY][0] === side + 'R_' ) {
+                if( isE( 1, pieceY ) && isE( 2, pieceY ) && isE( 3, pieceY ) )
+                    return [ 2, pieceY ];    
+            }
+        }
+        function getCastleRight() {
+            if( hasntMoved && layout[pieceY][7] === side + 'R_' ) {
+                if( isE( 5, pieceY ) && isE( 6, pieceY ) )
+                    return [ 6, pieceY ];    
+            }
+        }
     }
+
+
 
     //if location is onBoard
     function onB( x, y ) {
@@ -131,6 +399,14 @@ export default function pieceMovements( pieceX, pieceY, layout ) {
     //if location is vacant
     function isV( x, y ) {
         return onB( x, y ) && layout[y][x] === '';
+    }
+    //if location is enemy
+    function isE( x, y ) {
+        return onB( x, y ) && layout[y][x][0] !== side;
+    }
+    //if location is vacant or enemy
+    function isVE( x, y ) {
+        return isV( x, y ) || isE( x, y );
     }
 
 }
